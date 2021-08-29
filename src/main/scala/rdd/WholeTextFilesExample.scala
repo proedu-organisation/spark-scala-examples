204430d607af98ea568a6a72e2b9e9b31889b533
Proedu <professional.education.co@gmail.com>
2021-08-29 14:31:32 +0530

    Added textFile and wholeTextFile method examples

--- src/main/scala/rdd/WholeTextFilesExample.scala
+++ src/main/scala/rdd/WholeTextFilesExample.scala
@@ -15,9 +15,21 @@ object WholeTextFilesExample extends App {
   // Set Log level to ERROR
   sparkContext.setLogLevel("ERROR")
 
-  // Reading a text file.
-  val dataRDD = sparkContext.wholeTextFiles(file)
-  println(dataRDD)
+  // Read a directory containing text files.
+  val data = sparkContext.textFile(file)
+  //data.collect().foreach(println)
 
-  dataRDD.take(10).foreach(println)
+
+  // Reading a text file using wholeTextFiles method.
+  val dataRDD = sparkContext.wholeTextFiles(file)
+  //dataRDD.take(10).foreach(println)
+
+  // Reading text files with a matching pattern.
+  val patternRDD = sparkContext.textFile("src/main/resources/retail_db/categories-multipart/*00001")
+  //patternRDD.collect().foreach(println)
+
+  // Reading files from multiple directories
+  val multiDirRDD = sparkContext.textFile("src/main/resources/retail_db/categories-multipart/part-m-00001,src/main/resources/retail_db/categories/")
+  multiDirRDD.collect().foreach(println)
+
 }
